ad8a5f8df2ecb7927b934cbfe034f022fd7a3863
angelina-tsuboi <angelina.t1832@gmail.com>
2022-04-11 05:09:56 -0700

    feat: init dashbaord

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { AuthGuard } from './shared/guard/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
   { path: 'sign-in', component: SignInComponent },
   { path: 'register-user', component: SignUpComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   // { path: 'forgot-password', component: ForgotPasswordComponent },
   // { path: 'verify-email-address', component: VerifyEmailComponent },
 ];
