bd14293fb067892fdefde42046012e9bf5f2ba96
angelina-tsuboi <angelina.t1832@gmail.com>
2022-04-11 04:55:33 -0700

    feat: init firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAGUga7svVQimmUh7DFxj5PhsHqK_w0AI0",
+    authDomain: "rapid-e8c3a.firebaseapp.com",
+    databaseURL: "https://rapid-e8c3a.firebaseio.com",
+    projectId: "rapid-e8c3a",
+    storageBucket: "rapid-e8c3a.appspot.com",
+    messagingSenderId: "810575237114",
+    appId: "1:810575237114:web:d942f59cb3358747ba9e43",
+    measurementId: "G-GGXEKMH5F1"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAGUga7svVQimmUh7DFxj5PhsHqK_w0AI0",
+    authDomain: "rapid-e8c3a.firebaseapp.com",
+    databaseURL: "https://rapid-e8c3a.firebaseio.com",
+    projectId: "rapid-e8c3a",
+    storageBucket: "rapid-e8c3a.appspot.com",
+    messagingSenderId: "810575237114",
+    appId: "1:810575237114:web:d942f59cb3358747ba9e43",
+    measurementId: "G-GGXEKMH5F1"
+  }
 };
 
 /*
